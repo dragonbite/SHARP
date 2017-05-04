730ae0bb90f27cc3fadc3e1cefc1aff3211b4d46
Andrew Kwashnak <dragonbite@gmail.com>
2017-05-03 20:37:01 -0400

    Removed duplicate code from file merge

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 //$dbname='mccauction';		// if using different database, change it here
@@ -18,23 +17,3 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('<p>Error con
 $checkMyToken = 'JzJhVpKTnnhRqn6Vb5aXG7jpd67B6N8J6pxBvjWmBVKZC';
 
 ?>
-=======
-<?php
-
-//$dbname='mccauction';		// if using different database, change it here
-$dbname='auction';
-$dblogin='myauction';			// read-only login for the single table
-$dbuser=$dblogin;
-$dbpass='5z58d8RxJ6Y3eMeH';
-$dbhost='localhost';
-$error_message='';
-
-$conni = new mysqli($dbhost,  $dbuser, $dbpass, $dbname);
-
-$dbconn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('<p>Error connecting to mysql</p>');
-
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('<p>Error connecting to mysql</p>');
-$checkMyToken = 'JzJhVpKTnnhRqn6Vb5aXG7jpd67B6N8J6pxBvjWmBVKZC';
-
-?>
->>>>>>> 25337a4377b187df99edfe849eaaa98a18d1dcbf
